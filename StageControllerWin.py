12c1084a68c5a4c5d0d3d45d84cb38a5cf890bb7
Aaron Ray <aray@shore.mbari.org>
2019-06-18 14:07:59 -0700

    Added windows LTS300 controller

---
+++ StageControllerWin.py
@@ -0,0 +1,60 @@
+import zmq
+import time
+import sys
+from lts300 import LTS300
+import zmq
+import signal
+
+
+
+LTS300_X = '45874644'
+LTS300_Y = '45874637'
+
+keep_going = True
+
+def sigint_handler(signo, stack_frame):
+    global keep_going
+    keep_going = False
+
+def main():
+
+    ip_addr = '134.89.13.35'
+    port_num = 5557
+
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.setsockopt(zmq.CONFLATE,1)
+    socket.connect('tcp://%s:%d' % (ip_addr, port_num))
+    topicfilter = ''
+    socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
+
+    x_stage = LTS300(LTS300_X)
+    y_stage = LTS300(LTS300_Y)
+    x_stage.initialize()
+    y_stage.initialize()
+
+    time.sleep(2)
+    x_stage.device.StartPolling(50)
+    y_stage.device.StartPolling(50)
+    signal.signal(signal.SIGINT, sigint_handler)
+    
+    while keep_going:
+    
+        track_pos_str = socket.recv_string()
+        track_toks = track_pos_str.split(' ')
+        dx = float(track_toks[0])
+        dy = float(track_toks[1])
+        print('%f, %f' % (dx,dy))
+    
+        if abs(dx) > 30:
+            x_stage.jog(-dx/20)
+        if abs(dy) > 30:
+            y_stage.jog(dy/20)
+            
+        time.sleep(.1)
+        
+    x_stage.device.StopImmediate()
+    y_stage.device.StopImmediate()
+
+if __name__== '__main__':
+    main()
